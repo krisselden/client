3b243bc8bb358b0f2a1dd1817445da32cb9be17f
Kris Selden <kris.selden@gmail.com>
2020-06-29 20:49:09 -0700

    patch build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,7 +8,24 @@ const broccoliAssetRevDefaults = require('broccoli-asset-rev/lib/default-options
 module.exports = function(defaults) {
   let options = {
     inlineContent: {},
-    minifyJS: {},
+    'ember-cli-uglify': {
+      enabled: true,
+      async: true, // run uglify in parallel
+
+      uglify: {
+        compress: {
+          // this is adversely affects heuristics for IIFE eval
+          'negate_iife': false,
+          // limit sequences because of memory issues during parsing
+          sequences: 0,
+        },
+        mangle: false,
+        output: {
+          // no difference in size and much easier to debug
+          semicolons: false,
+        },
+      }
+    },
     minifyCSS: {},
     fingerprint: {
       extensions: broccoliAssetRevDefaults.extensions.concat(['svg'])
@@ -18,27 +35,27 @@ module.exports = function(defaults) {
     }
   };
 
-  if (parseFlag('FAVICON', true)) {
-    options.inlineContent['snippets/favicon'] = 'app/snippets/favicon.html';
-  }
+  // if (parseFlag('FAVICON', true)) {
+  //   options.inlineContent['snippets/favicon'] = 'app/snippets/favicon.html';
+  // }
 
-  if (parseFlag('EXTERNAL_FONTS', true)) {
-    options.inlineContent['snippets/external-fonts'] = 'app/snippets/external-fonts.html';
-  }
+  // if (parseFlag('EXTERNAL_FONTS', true)) {
+  //   options.inlineContent['snippets/external-fonts'] = 'app/snippets/external-fonts.html';
+  // }
 
-  if (parseFlag('REPORT_ERRORS', env === 'production')) {
-    options.inlineContent['snippets/trackjs'] = 'app/snippets/trackjs.html';
-  }
+  // if (parseFlag('REPORT_ERRORS', env === 'production')) {
+  //   options.inlineContent['snippets/trackjs'] = 'app/snippets/trackjs.html';
+  // }
 
   options.inlineContent['snippets/perf-utils'] = 'app/snippets/perf-utils.js';
 
-  options.minifyJS.enabled = parseFlag('MINIFY_JS', env === 'production');
+  // options['ember-cli-uglify'].enabled = parseFlag('MINIFY_JS', env === 'production');
 
-  options.minifyCSS.enabled = parseFlag('MINIFY_CSS', env === 'production');
+  options.minifyCSS.enabled = true;
 
-  options.sourcemaps.enabled = parseFlag('SOURCEMAPS', env !== 'production');
+  options.sourcemaps.enabled = false;
 
-  options.fingerprint.enabled = parseFlag('FINGERPRINT', env === 'production');
+  options.fingerprint.enabled = false;
 
   let app = new EmberApp(defaults, options);
 
